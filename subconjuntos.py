3fa6978aed59ff9beacfce857fe9a2d782ad124f
José Carlos González González <jgonzal@ull.edu.es>
2016-10-16 09:21:09 +0100

    sol_array

--- subconjuntos.py
+++ subconjuntos.py
@@ -3,9 +3,11 @@ def suma_subconjuntos(entrada):
     subconjuntos = 0
     for t in range(1, N+1):
         for i in range(1,N-t+2):
-#            print(i,t,check(i,t,entrada))
+            sol_array=[]
+            sol_sum=0
+            print(i,t,check(i,t,entrada))
             subconjuntos += 1
-#    print("Encontrados %d subconjuntos con tamanio de entrada %d y complejidad %d %d" % (subconjuntos,N,2**(N/2),(2**(N/2))*N))
+    print("Encontrados %d subconjuntos con tamanio de entrada %d y complejidad %d %d" % (subconjuntos,N,2**(N/2),(2**(N/2))*N))
     print("%d;%d;%d;%d" % (N,subconjuntos,N**2,N**3))
 
 
@@ -18,7 +20,8 @@ def check(i,t,entrada):
             return(entrada[i-1],check(j,t-1,entrada))
 
 
-lista = []
-for i in range(1,3000):
-    lista.append(i)
+
+for i in range(5):
+    lista=range(i*-1,i+1)
+    print(lista)
     suma_subconjuntos(lista)

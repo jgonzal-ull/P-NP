9e285d3082ea2aa8c3cd9e946f6751a42d074418
José Carlos González González <jgonzal@ull.edu.es>
2016-10-19 06:50:59 +0100

    realizando aproximación polinómica

--- subconjuntos.py
+++ subconjuntos.py
@@ -13,6 +13,7 @@ subconjuntos_correctos = 0
 
 valores_llamadas = {}
 sumas_llamadas = {}
+indices_menos_uno = {}
 ind_actual = 0
 
 def mensaje(cadena):
@@ -20,9 +21,8 @@ def mensaje(cadena):
 
     if verbose == 1:
         print(cadena)
-
 #@profile
-def suma_subconjuntos():
+def suma_subconjuntos_exponencial():
     global lista, sol_actual, sol_suma, N, subconjuntos_totales, subconjuntos_correctos, verbose
 
     global llamadas, valores_llamadas, sumas_llamadas, ind_actual
@@ -36,39 +36,30 @@ def suma_subconjuntos():
         for i in range(N-t+1):
             sol_actual = [lista[i]]
             sol_suma=lista[i]
-            ind_actual = 2**i
-            check_polinomial(i,t)
+            check_exponencial(i,t)
 
-#@profile
-def check_polinomial(i,t):
+def suma_subconjuntos_polinomial():
     global lista, sol_actual, sol_suma, N, subconjuntos_totales, subconjuntos_correctos, verbose
 
-    global llamadas, ind_actual
+    global llamadas, valores_llamadas, sumas_llamadas, ind_actual, indices_menos_uno
 
-    if llamadas.has_key(2**i):
-        llamadas[2**i] += 1
-    else:
-        llamadas[2**i] = 1
-    if t == 1:
-        if sol_suma == 0:
-            mensaje("CORRECTO")
-            mensaje(sol_actual)
-            subconjuntos_correctos += 1
-            subconjuntos_totales += 1
-        else:
-            mensaje(sol_actual)
-            subconjuntos_totales += 1
-        valores_llamadas[ind_actual] = [x for x in sol_actual]
-        sumas_llamadas[ind_actual] = sol_suma
-    else:
-        for j in range(i + 1, N):
-            ind_actual += 2**j
-            sol_actual.append(lista[j])
-            sol_suma += lista[j]
-            check_polinomial(j, t - 1)
-            sol_actual = sol_actual[:-1]
-            sol_suma -= lista[j]
-            ind_actual -= 2**j
+    subconjuntos_totales = 0
+    subconjuntos_correctos = 0
+    llamadas = {}
+    valores_llamadas = {}
+    sumas_llamadas = {}
+    for i in range(N):
+        ind_actual = 2**i
+        valores_llamadas[ind_actual]=[lista[i]]
+        sumas_llamadas[ind_actual]=lista[i]
+        indices_menos_uno[i] = ind_actual
+    for t in range(2, N + 1):
+        for i in range(N - t + 1):
+            for j in range(i + 1, N):
+                ind_actual = 2**i + indices_menos_uno[j]
+                valores_llamadas[ind_actual] = [lista[i]] + [x for x in valores_llamadas[indices_menos_uno[j]]]
+                sumas_llamadas[ind_actual] = lista[i] + sumas_llamadas[indices_menos_uno[j]]
+                indices_menos_uno[j] = ind_actual
 
 
 #@profile
@@ -103,7 +94,7 @@ for i in range(3,4):
     mensaje("Comenzando problema: ")
     N = len(lista)
     tiempo_inicial = time()
-    suma_subconjuntos()
+    suma_subconjuntos_polinomial()
     tiempo_final = time()
     tiempo = tiempo_final - tiempo_inicial
     print("N = %d; Subconjuntos_totales = %d;Subconjuntos_correctos = %d; Tiempo = %f; llamadas_diferentes = %d" % (N, subconjuntos_totales, subconjuntos_correctos, tiempo, len(llamadas)))

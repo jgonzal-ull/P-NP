e62c0590667efd059564c60736a1628db3eb86db
José Carlos González González <jgonzal@ull.edu.es>
2016-12-22 20:17:56 +0000

    Numeros Naturales

--- funciones.py
+++ funciones.py
@@ -1,16 +1,30 @@
 # coding=utf-8
 
+verbose = 0
+
+# 0 Sólo errores graves de funcionamiento
+# 1 Errores y mensajes resumidos detalle medio
+# 2 Detalle máximo
+def mensaje(cadena, nivel):
+    global verbose
+    if nivel <= verbose:
+        print(cadena)
+
+
 def get_subconjuntos(conjunto):
     longitud = len(conjunto)
+    conjuntos = 0
     for long in range(1,longitud+1):  #logitudes de los conjuntos
         for pos in range(0,longitud-long+1): #primer campo del conjunto
-            print("Longitud: %d    Posición: %d" % (long, pos))
+#            print("Longitud: %d    Posición: %d" % (long, pos))
             elemento=[]
-            for pref in range(pos, pos+long-1):
+            for pref in range(pos, pos+long):
                 elemento.append(conjunto[pref])
-            for sig in range(pos+long-1,longitud):
-                elemento.append(conjunto[sig])
-                print(elemento)
-                elemento.pop()
+            conjuntos += 1
+#            print(elemento)
+    return(conjuntos)
 
-
+def get_subconjuntos_diferentes(conjunto):
+    conj = set(conjunto)
+    conjuntos_diferentes = len(conj)
+    return (conjuntos_diferentes)
--- suma_numeros_naturales.py
+++ suma_numeros_naturales.py
@@ -1,47 +1,38 @@
 # coding=utf-8
 import numpy as np
 from funciones import *
-from time import time
 
-verbose = 0
 Nnum = 100
 
-solucion = np.zeros((1, Nnum + 1))
+solucion = np.zeros((1, Nnum))
+solucion_original = np.zeros((1, Nnum))
+solucion_original_profile = np.zeros((1, Nnum))
 numeros = np.zeros((1,Nnum))
 indices = np.zeros((1,Nnum))
-
 secuencia=[]
 
 for i in range(0,Nnum):
     numeros[0][i] = i
-# 0 Sólo errores graves de funcionamiento
-# 1 Errores y mensajes resumidos detalle medio
-# 2 Detalle máximo
-def mensaje(cadena, nivel):
-    global verbose
-    if nivel <= verbose:
-        print(cadena)
-
 def set_solucion(posicion, valor):
     global solucion, secuencia
-    solucion[0][posicion] = valor
+    solucion[0][int(posicion)] = valor
     mensaje("SS%d:%f" % (posicion, valor),2)
     secuencia.append("SS%d:%f" % (posicion, valor))
 def set_indice(posicion, valor):
     global indices, secuencia
-    indices[0][posicion] = valor
+    indices[0][int(posicion)] = valor
     mensaje("SI%d:%f" % (posicion, valor),2)
     secuencia.append("SI%d:%f" % (posicion, valor))
 def set_numero(posicion, valor):
     global numeros, secuencia
-    numeros[0][posicion] = valor
+    numeros[0][int(posicion)] = valor
     mensaje("SN%d:%f" % (posicion, valor),2)
     secuencia.append("SN%d:%f" % (posicion, valor))
 def read_solucion(posicion):
     global solucion, secuencia
-    mensaje("RS%d:%f" % (posicion, solucion[0][posicion]),2)
-    secuencia.append("RS%d:%f" % (posicion, solucion[0][posicion]))
-    return(solucion[0][posicion])
+    mensaje("RS%d:%f" % (posicion, solucion[0][int(posicion)]),2)
+    secuencia.append("RS%d:%f" % (posicion, solucion[0][int(posicion)]))
+    return(solucion[0][int(posicion)])
 def read_indice(posicion):
     global indices, secuencia
     mensaje("RI%d:%f" % (posicion, indices[0][posicion]),2)
@@ -49,60 +40,84 @@ def read_indice(posicion):
     return(indices[0][posicion])
 def read_numero(posicion):
     global numeros, secuencia
-    mensaje("RN%d:%f" % (posicion, numeros[0][posicion]),2)
-    secuencia.append("RN%d:%f" % (posicion, numeros[0][posicion]))
-    return(numeros[0][posicion])
+    mensaje("RN%d:%f" % (posicion, numeros[0][int(posicion)]),2)
+    secuencia.append("RN%d:%f" % (posicion, numeros[0][int(posicion)]))
+    return(numeros[0][int(posicion)])
 
 def suma_naturales_original(N):
-    global solucion
+    global solucion_original
 
-    for i in range(1, N + 2):
-        solucion[0][i - 1] = 0
-        for j in range(0, i):
-            solucion[0][i - 1] += j
+    i=1
+    while(i <= N):
+        solucion_original[0][i - 1] = 0
+        j = 1
+        while(j<=i):
+            solucion_original[0][i - 1] += j
+            j += 1
+        i += 1
 
 def suma_naturales_profile(N):
-    global solucion
+    set_indice(0,1)  # i=1
+    while(read_indice(0)<=N):  # while i<=N
+        set_solucion(read_indice(0) - 1, 0)  # solucion_original[0][i - 1] = 0
+        set_indice(1,1)    # j = 1
+        while (read_indice(1) <= read_indice(0)): #  while(j<=i):
+            set_solucion(read_indice(0)-1,read_solucion(read_indice(0)-1) + read_numero(read_indice(1))) #solucion_original[0][i - 1] += j
+            set_indice(1,read_indice(1)+read_numero(1)) #j += 1
+        set_indice(0,read_indice(0)+read_numero(1)) # i += 1
 
-    set_indice(0,1)
-    for i in range(1, N + 2):
-        read_indice(0)
-        set_solucion(i - 1, 0)
-        set_indice(1,0)
-        for j in range(0, i):
-            read_indice(1)
-            set_solucion(i-1,read_solucion(i-1) + read_numero(j))
-            set_indice(1,read_indice(1)+read_numero(1))
-        set_indice(0,read_indice(0)+read_numero(1))
+
+
+def suma_naturales_original_optimizado1(N):
+    global solucion_original
+
+    i=1
+    while(i <= N):
+        suma = 0
+        j = 1
+        while(j<=i):
+            suma += j
+            j += 1
+        solucion_original[0][i - 1] = suma
+        i += 1
+
+
+def suma_naturales_profile_optimizado1(N):
+    set_indice(0,1)  # i=1
+    while(read_indice(0)<=N):  # while i<=N
+        set_indice(2, 0)  # suma = 0
+        set_indice(1,1)    # j = 1
+        while (read_indice(1) <= read_indice(0)): #  while(j<=i):
+            set_indice(2, read_indice(2)+read_numero(read_indice(1))) # suma += j
+            set_indice(1,read_indice(1)+read_numero(1)) #j += 1
+        set_solucion(read_indice(0)-1,read_indice(2)) #solucion_original[0][i - 1] = suma
+        set_indice(0,read_indice(0)+read_numero(1)) # i += 1
 
 def suma_naturales_check(N):
-    global solucion
-
-    if solucion[0][1] <> 1.0:
-        mensaje("Error verificando 1", 0)
-        return (False)
-    if solucion[0][2] <> 3.0:
-        mensaje("Error verificando 1", 0)
-        return (False)
-    if solucion[0][3] <> 6.0:
-        mensaje("Error verificando 1", 0)
-        return (False)
-    if solucion[0][4] <> 10.0:
-        mensaje("Error verificando 1", 0)
-        return (False)
-    return (True)
+    global solucion, solucion_original
+#    print(solucion)
+#    print(solucion_original)
+#    print(solucion == solucion_original)
+    return(solucion == solucion_original)
 
 
-#suma_naturales_original(N)
-print("N;Real;N2;N3;N4")
-for N in range(2,3):
+print("N;Real;Diferentes;N2;N3;N4;Conjuntos;Diferentes")
+for N in range(2,20):
     secuencia = []
+    suma_naturales_original(N)
     suma_naturales_profile(N)
-    suma_naturales_check(N)
-    print("%d;%d;%d;%d;%d" %(N,len(secuencia),N*N,N*N*N,N*N*N*N))
-    print(secuencia)
-    get_subconjuntos([1,2,3,4,5])
+    if np.all(suma_naturales_check(N)[0]):
+        mensaje("%d;%d;%d;%d;%d;%d;%d" %(N,len(secuencia),get_subconjuntos_diferentes(secuencia), \
+                                         N*N,N*N*N,N*N*N*N,get_subconjuntos(secuencia), \
+                                        ),0)
 
+    secuencia = []
+    suma_naturales_original_optimizado1(N)
+    suma_naturales_profile_optimizado1(N)
+    if np.all(suma_naturales_check(N)[0]):
+        mensaje("%d;%d;%d;%d;%d;%d;%d" %(N,len(secuencia),get_subconjuntos_diferentes(secuencia), \
+                                         N*N,N*N*N,N*N*N*N,get_subconjuntos(secuencia), \
+                                        ),0)
 
 
 
